d1d240afd2d889101472f9b927aa4b80acccce82
mobinafaraji <mobna_faraji@yahoo.com>
2025-07-08 15:03:29 +0200

    Add vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+// vite.config.js
+
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/Italy_air_quality_/', // Replace with your actual repository name
+  build: {
+    outDir: 'dist',
+  },
+});
